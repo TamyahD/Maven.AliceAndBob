bf6addf2170e5a58cbd7fb0fe3476f00886f42de
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-06-24 07:57:25 -0400

    additional methods in Main class & method calls in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
     public static void main(String[] args ){
+        isAliceOrBob(getInput());
+    }
 
+    public static String getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+        return name;
+    }
+
+    public static void isAliceOrBob(String n) {
+        String user01 = "Bob";
+        String user02 = "Alice";
+        if (n.equals(user01) | (n.equals(user02))) {
+            System.out.println("Hello how are you :)");
+        }
+        else {
+           System.out.println("I don't care how you are...");
+        }
     }
 }
